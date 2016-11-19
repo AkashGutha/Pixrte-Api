bb5a4324e16460338c6564e2569dd03190ca85d9
AkashGutha <akash.gutha@outlook.com>
2016-11-19 16:24:18 +0530

    change from naem to username

--- routes/api/users.js
+++ routes/api/users.js
@@ -19,48 +19,47 @@ var settings = require('../../config/settings.json');
 
 Users.get('/', function (req, res, next) {
 
-    // if query is empty.
-    if (!req.query.name) res.status(400).end();
+	// if query is empty.
+	if (!req.query.username) res.status(400).end();
 
-    // if the request name is all
-    else if (req.query.name === "all") {
-        if (req.user.isAdmin === true) {
+	// if the request name is all
+	else if (req.query.username === "all") {
+		if (req.user.isAdmin === true) {
+			UserDB.findAll(function (err, users) {
+				if (err)
+					res.status(500).end();
+				else res.status(200).json(users);
+			});
+		} else res.status(403).json(seeds.NotAuthorized);
+	}
 
-            UserDB.findAll(function (err, users) {
-                if (err)
-                    res.status(500).end();
-                else res.status(200).json(users);
-            });
-        } else res.status(403).json(seeds.NotAuthorized);
-    }
+	// if the request name is me
+	else if (req.query.username === "me") {
+		UserDB.find(req.user.username, function (err, user) {
+			if (err)
+				res.status(500).end();
+			if (user)
+				res.status(200).json(user);
+			else res.status(403).json(seeds.UserNotFound);
+		});
+	}
 
-    // if the request name is me
-    else if (req.query.name === "me") {
-        UserDB.find(req.user.username, function (err, user) {
-            if (err)
-                res.status(500).end();
-            if (user)
-                res.status(200).json(user);
-            else res.status(403).json(seeds.UserNotFound);
-        });
-    }
-
-    // if the request name is a anything else
-    else {
-        if (req.user.username === req.query.name || req.user.isAdmin === true) {
-            UserDB.find(req.query.name, function (err, user) {
-                if (err)
-                    res.status(500).end();
-                if (user)
-                    res.status(200).json(user);
-                else res.status(403).json(seeds.UserNotFound);
-            });
-        } else if (req.user.username !== req.query.name) {
-            res.status(403).json(seeds.NotAuthorized);
-        } else {
-            res.status(500).end();
-        }
-    }
+	// if the request name is a anything else
+	else {
+		if (req.user.username === req.query.username || req.user.isAdmin === true) {
+			UserDB.find(req.query.username, function (err, user) {
+				if (err)
+					res.status(500).end();
+				if (user)
+					res.status(200).json(user);
+				else res.status(403).json(seeds.UserNotFound);
+			});
+		} else if (req.user.username !== req.query.username) {
+			res.status(403).json(seeds.NotAuthorized);
+		} else {
+			res.status(500).end();
+		}
+	}
 });
 
 
@@ -70,17 +69,17 @@ Users.get('/', function (req, res, next) {
 //======================================================================
 
 
-Users.put('/:username', function (req, res, next) {
+Users.put('/', function (req, res, next) {
 
-    var newData = req.body;
+	var newData = req.body;
 
-    //validate the new Data //code here
+	//validate the new Data //code here
 
-    //if valid update the user data
-    UserDB.update(req.user, newData, function (err) {
-        if (err) res.status(400).end();
-        else res.status(200).json(seeds.UserUpdated);
-    });
+	//if valid update the user data
+	UserDB.update(req.body.username, newData, function (err) {
+		if (err) res.status(400).end();
+		else res.status(200).json(seeds.UserUpdated);
+	});
 
 });
 
@@ -89,13 +88,13 @@ Users.put('/:username', function (req, res, next) {
 //======================================================================
 
 
-Users.delete('/:username', function (req, res, next) {
+Users.delete('/', function (req, res, next) {
 
-    if (req.user.username === req.params.username || req.isAdmin === true)
-        UserDB.remove(req.user, function (err, user) {
-            return res.status(200).json(user);
-        });
-    else res.status(500).end();
+	if (req.user.username === req.query.username || req.isAdmin === true)
+		UserDB.remove(req.user.username, function (err, user) {
+			return res.status(200).json(user);
+		});
+	else res.status(500).end();
 });
 
 //======================================================================
