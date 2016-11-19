1cf7a9a17f21bb657ce84beeedc14a15ed4b2624
AkashGutha <akash.gutha@outlook.com>
2016-11-19 09:01:42 +0530

    user data validation

---
+++ helpers/DataValidators/UserDataValidator.js
@@ -0,0 +1,19 @@
+var username_regex = /^[a-z0-9_]{4,20}$/;
+var password_regex = /^[A-Za-z0-9!@#$%^&*()_]{6,20}$/;
+var email_regex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+
+function validateUsername(name) {
+	return name_regex.test(name);
+}
+
+function validatePassword(password) {
+	return password_regex.test(password);
+}
+
+function validateEmail(email) {
+	return email_regex.test(email);
+}
+
+exports.validateUsername = validateUsername;
+exports.validatePassword = validatePassword;
+exports.validateEmail = validateEmail;
